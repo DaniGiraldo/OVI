86c9b1a5b8228d3c0631c1d45c7f86c4d6204904
Daniela Giraldo <danielagq1006@gmail.com>
2018-04-14 17:39:22 -0500

    readings and videos

---
+++ src/js/counter.js
@@ -0,0 +1,11 @@
+function counter (){
+	/*if (localStorage.contador) {
+		localStorage.contador = Number(localStorage.contador) + 1;
+	} else {
+		localStorage.contador = 1;
+	}*/
+
+	$('.visitor_counter').append("<span><strong>El sitio ha sido visitado " + localStorage.contador + " veces</strong></span>");
+}
+
+console.log('counter.js loaded');
--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,41 @@ function main (){
 	$('#authors').on('click', function(){
 		$('.container').load('authors.html');
 	});
+
+	/*readings*/
+
+	$('#reading1').on('click', function(){
+		$('.container').load('readings/config_env.html');
+	});
+
+	$('#reading2').on('click', function(){
+		$('.container').load('readings/create_bd.html');
+	});
+
+	/*$('#reading2').on('click', function(){
+		$('.container').load('readings/create_user.html');
+	});*/
+
+	$('#reading3').on('click', function(){
+		$('.container').load('readings/connection_php_mysql.html');
+	});
+
+	$('#reading4').on('click', function(){
+		$('.container').load('readings/commands.html');
+	});
+
+	$('#reading5').on('click', function(){
+		$('.container').load('readings/test.html');
+	});
+
+	/*videos*/
+	$('#video1').on('click', function(){
+		$('.container').load('videos/video1.html');
+	});
+
+	$('#video2').on('click', function(){
+		$('.container').load('videos/video2.html');
+	});
 }
 
 console.log('main.js loaded');
