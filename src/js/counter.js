33c57a65a8df2e61cf15240e9494cd70b94abd45
Daniela Giraldo <danielagq1006@gmail.com>
2018-04-15 14:24:41 -0500

    Last Changes

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,9 +1,9 @@
 function counter (){
-	/*if (localStorage.contador) {
+	if (localStorage.contador) {
 		localStorage.contador = Number(localStorage.contador) + 1;
 	} else {
 		localStorage.contador = 1;
-	}*/
+	}
 
 	$('.visitor_counter').append("<span><strong>El sitio ha sido visitado " + localStorage.contador + " veces</strong></span>");
 }
